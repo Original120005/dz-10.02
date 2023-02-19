035c8671e7fd309ea6152d078d3b32cfe764c959
Дима <@DESKTOP-TC925PR>
2023-02-19 20:08:43 +0200

    Add project files.

---
+++ Matrix.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dz_10._02
+{
+    public class Matrix
+    {
+    }
+}
